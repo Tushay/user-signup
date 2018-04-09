d62622b6fe561461df6237b6f9510417430c7ec5
Adam Clark <yahsut123@gmail.com>
2018-04-09 11:32:37 -0500

    almost finished, need help on welcome page

--- main.py
+++ main.py
@@ -1,25 +1,55 @@
 from flask import Flask, request, redirect, render_template
 import cgi
-import os
 
 app = Flask(__name__)
+
 app.config['DEBUG'] = True
 
-@app.route ("/")
-def index():
-    return render_template('homepage.html')
-
-
-@app.route("/", methods=['POST'])
-def user():
+@app.route('/error', methods=['POST'])
+def validate():
     username = request.form['username']
     password = request.form['password']
-    verify_password = request.form['verify_password']
+    verify_pass = request.form['verify-pass']
+    email = request.form['email']
 
+    username_error = ''
+    password_error = ''
+    email_error = ''
+    verify_error = ''
+
+
+    if (not username) or (username.strip() == '') or ((len(username) > 20 or len(username) < 3)) or (' ' in username):
+        username_error = "That's not a valid username"
+
+    if (not password) or (password.strip() == '') or ((len(password) > 20 or len(password) < 3)) or (' ' in password):
+            password_error = "That's not a valid password"
+
+    if (password != verify_pass) or (not verify_pass):
+        verify_error = "Passwords don't match"
+
+    if email == '':
+        email = ''
+    else:
+        if ('@' and '.') not in email:
+            email_error = "That's not a valid email"
     
+    if (not username_error) and (not password_error) and (not verify_error) and (not email_error):
+        username = str(username)        
+        return redirect('/welcome?user={0}'.format(username))
+    else:
+        return render_template('edit.html', username_error = username_error, 
+            password_error = password_error, verify_error = verify_error,
+            email_error = email_error)    
 
-
-
+@app.route('/welcome', methods=['GET', 'POST'])
+def success():
+    username = ''
+    if request.method == 'POST':
+        username = request.form['username']
+    return render_template('welcome.html', username = username)
+@app.route('/')
+def index():
+    return render_template('edit.html')
 
 
 app.run()
